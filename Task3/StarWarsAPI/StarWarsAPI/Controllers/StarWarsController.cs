6749bb91b872d9b4ffaef0100529a6f6fa34148c
Hafeez Shaik <afiz.ahmed@gmail.com>
2020-02-18 15:55:04 +0530

    Task 3 changes

---
+++ Task3/StarWarsAPI/StarWarsAPI/Controllers/StarWarsController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using StarWars.Data;
+using StarWars.Models;
+
+namespace StarWarsAPI.Controllers
+{
+    public class StarWarsController : ApiController
+    {
+        [Route("api/StarWars/GetCrawls")]
+        [HttpGet]
+        public IHttpActionResult GetOpeningCrawl()
+        {
+            StarWarsContext _context = new StarWarsContext();
+            try
+            {
+                List<Film> objFilmList = _context.GetOpeningCrawl();
+                return Ok(objFilmList);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        [Route("api/StarWars/MostAppearedPerson")]
+        [HttpGet]
+        public IHttpActionResult GetMostAppearedPerson()
+        {
+            StarWarsContext _context = new StarWarsContext();
+            try
+            {
+                List<Person> objPersonList = _context.GetMostAppearedPerson();
+                return Ok(objPersonList);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        [Route("api/StarWars/MostAppearedSpecies")]
+        [HttpGet]
+        public IHttpActionResult GetMostAppearedSpecies()
+        {
+            StarWarsContext _context = new StarWarsContext();
+            try
+            {
+                List<Species> objSpeciesList = _context.GetMostAppearedSpecies();
+                return Ok(objSpeciesList)
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
