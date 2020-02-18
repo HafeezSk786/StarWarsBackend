e7166f4812e24bb489ad8b205f8c1925140b0e5e
Hafeez Shaik <afiz.ahmed@gmail.com>
2020-02-18 15:57:13 +0530

    Task4 changes

---
+++ Task4/StarWarsAPI/StarWars.Models/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarWars.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string BirthYear { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Edited { get; set; }
+        public string EyeColor { get; set; }
+        public string Gender { get; set; }
+        public string HairColor { get; set; }
+        public string Height { get; set; }
+        public int HomeWorld { get; set; }
+        public string Mass { get; set; }
+        public string Name { get; set; }
+        public string SkinColor { get; set; }
+        public int Count { get; set; }
+    }
+}
---
+++ Task4/StarWarsAPI/StarWarsAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace StarWarsAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
